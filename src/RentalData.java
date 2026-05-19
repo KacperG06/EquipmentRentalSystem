123fa4ee5c71c738d68bd52bff5081c66c57f4df
Kacperg06 <frkacper06@gmail.com>
2026-05-19 20:20:46 +0200

    Dodano pola do klasy management i zarys metod. zrobiono zarys metody load() i save() dla klas UserData, EquipmentData, RentalData

--- src/RentalData.java
+++ src/RentalData.java
@@ -1,3 +1,22 @@
-public class RentalData implements DataManager{
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentalData implements DataManager<Rental>{
     private String file;
+
+    public RentalData(String file){
+        this.file = file;
+    }
+
+    @Override
+    public void save(List<Rental> rentals) {
+        System.out.println("Zapisano wypozyczenia: "+file);
+
+    }
+
+    @Override
+    public List<Rental> load() {
+        System.out.println("Wczytywanie wypożyczen: "+file);
+        return new ArrayList<>();
+    }
 }
