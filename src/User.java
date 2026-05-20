924722c49ac023f82be4b1186267cc3fcf97de71
Kacperg06 <frkacper06@gmail.com>
2026-05-20 17:29:17 +0200

    Uzupelniene klasy User

--- src/User.java
+++ src/User.java
@@ -1,2 +1,48 @@
 public class User {
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String email;
+    private String pesel;
+    private int age;
+    private boolean driverLicense;
+
+
+    public User(String name, String surname, String phoneNumber, String email,String pesel, int age, boolean driverLicense){
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.pesel = pesel;
+        this.age = age;
+        this.driverLicense = driverLicense;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean hasDriverLicense() {
+        return driverLicense;
+    }
 }
