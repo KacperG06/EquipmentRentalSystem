d2a6512415f67f1422e350fcad51ac9ae5f9b560
Kacperg06 <frkacper06@gmail.com>
2026-05-20 17:32:26 +0200

    Dodanie metody load() do USerData

--- src/UserData.java
+++ src/UserData.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +20,25 @@ public class UserData implements DataManager<User> {
 
     @Override
     public List<User> load() {
-        System.out.println("Wczytywanie danych uzytkownikow: "+file);
-        return new ArrayList<>();
+        List<User> userList = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String line;
+            while((line = reader.readLine()) != null){
+                String[] data = line.split(";");
+                int age = Integer.parseInt(data[5]);
+                boolean driverLicense = Boolean.parseBoolean(data[6]);
+                User newUser = new User(data[0], data[1], data[2], data[3], data[4], age, driverLicense);
+                userList.add(newUser);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie znaleziono pliku!");
+        }
+        catch (IOException e){
+            System.out.println("Coś poszło nie tak!");
+        }
+
+        return userList;
     }
 }
