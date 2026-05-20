22ae24f4b66fa23750d3a49ac4b9dc8bb19afe27
Kacperg06 <frkacper06@gmail.com>
2026-05-20 17:14:25 +0200

    Aktualizacja pliku Rental.java

--- src/Rental.java
+++ src/Rental.java
@@ -1,2 +1,43 @@
+import java.time.LocalDateTime;
+
 public class Rental {
+    private int id;
+    private User user;
+    private Equipment equipment;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private double totalCost;
+
+    public Rental(int id, User user, Equipment equipment, LocalDateTime startDate, LocalDateTime endDate, double totalCost){
+        this.id = id;
+        this.user = user;
+        this.equipment = equipment;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.totalCost = totalCost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Equipment getEquipment() {
+        return equipment;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
 }
